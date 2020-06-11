f1ca36fe0fd4940ad4a5b2eb3defdbf34c37b20c
bfiles3 <65058048+bfiles3@users.noreply.github.com>
2020-06-11 15:33:55 -0400

    Added high score submission form

--- script.js
+++ script.js
@@ -1,122 +1,114 @@
+window.onload = function () {
+    createQuiz();
+    startTimer();
+};
+
 var progress = 0;
 var correct = 0;
-var quiz, question, questions, answer, answers, A, B, C;
 
 var questions = [
-{ 
-    question: "What does HTML stand for?",
+    {
+        question: "What does HTML stand for?",
         A: "Hyperbole Madeup Language",
         B: "Hypertext Markup Language",
         C: "Hero Market Language",
         answer: "B"
-},
-{   
-    question: "What are the return options for a boolean?",
+    },
+    {
+        question: "What are the return options for a boolean?",
         A: "Yes, No",
         B: "Confirm, Deny",
         C: "True, False",
         answer: "C"
-},
-{   question: "Which code would you use to create a button in HTML?",
-        A: "<button>",
-        B: "{button}",
-        C: "button()",
-        answer: "A"
-},
-{
-    question: "Which is the largest heading?",
+    },
+    {
+        question: "Which is the largest heading?",
         A: "h10",
         B: "h6",
         C: "h1",
         answer: "C"
-},
-{
-    question: "Where in an HTML document would you link a CSS stylesheet?",
-        A: "In the <body> tag",
-        B: "In the <head> tag",
-        C: "In the <footer> tag",
-        answer: "B"
-},
-{ 
-    question: "What is an example of a self-closing tag?",
-        A: "The <br> tag",
-        B: "The <a> tag",
-        C: "The <html> tag",
-        answer: "A"
-},
-{
-    question: "Which type of document is best to use to format colors on a webpage?",
+    },
+    {
+        question: "Which type of document is best to use to format colors on a webpage?",
         A: "HTML",
         B: "CSS",
         C: "JavaScript",
-        answer: "B" 
-},
-{
-    question: "What does CSS stand for?",
+        answer: "B"
+    },
+    {
+        question: "What does CSS stand for?",
         A: "Computer Styling Sheets",
         B: "Cascading Style Sheets",
         C: "Coloring Styled Sheets",
         answer: "B"
-},
-{
-    question: "What HTML element represents JavaScript?",
-        A: "<script>",
-        B: "<style>",
-        C: "<rel=stylesheet>",
-        answer: "A"
-},
-{
-    question: "How would you create an alert in JavaScript?",
+    },
+
+    {
+        question: "How would you create an alert in JavaScript?",
         A: "box('message')",
         B: "alert{'message'}",
         C: "alert('message')",
         answer: "C"
-}];
+    }];
 
-function get (x) {
-    return document.getElementById(x);
+function createQuiz() {
+    quiz = document.getElementById("quiz");
+    question = questions[progress].question;
+    // display the question
+    quiz.innerHTML = "<br /> <h3>" + question + "</h3><br />";
+    // display the answer options
+
+    quiz.innerHTML += "<label> <input type='radio' name='answers' value='A'> " + questions[progress].A + "</label><br>";
+    quiz.innerHTML += "<label> <input type='radio' name='answers' value='B'> " + questions[progress].B + "</label><br>";
+    quiz.innerHTML += "<label> <input type='radio' name='answers' value='C'> " + questions[progress].C + "</label><br><br>";
+    quiz.innerHTML += "<button class='btn' onClick='checkAnswer()'>Submit Answer</button>";
+    document.getElementById("progress").innerHTML = "Question " + (progress + 1) + " of " + questions.length;
+
+
+};
+
+// checks if answer matches the correct choice
+function checkAnswer() {
+    var answers = document.getElementsByTagName('input');
+    var value;
+    for (var i = 0; i < answers.length; i++) {
+        if (answers[i].type === 'radio' && answers[i].checked) {
+            // get value, set checked flag or do whatever you need to
+            value = answers[i].value;
+
+            if (value == questions[progress].answer) {
+                correct++
+            }
+        }
+    }
+    // changes position of which question user is on
+    if (progress < questions.length) 
+      
+    progress++ 
+        
+    console.log(progress)
+    if (progress == questions.length) {
+        showForm();
+    }
+    else {createQuiz()}  
+    console.log(correct);
+
+};
+
+var buttons = document.querySelectorAll(".btn");
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', checkAnswer, false);
 }
 
-function createQuiz () {
-    quiz = get("#quiz");
-    if (progress >= questions.length){
-        quiz.innerHTML = "Your score is" + correct;
-        get("#progress").innerHTML = "Game Over!";
-    progress = 0;
-    correct = 0;
-    return false; 
-    }
-get ("#progress") = "Question" + (progress + 1) + "of" + questions.length;
-question = questions[progress].question;
-A = questions[progress].A;
-B = questions[progress].B;
-B = questions[progress].C;
-// display the question
-quiz.innerHTML = "<h3>"+question+"</h3>";
-// display the answer options
-// the += appends to the data we started on the line above
-quiz.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+A+"</label><br>";
-quiz.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+B+"</label><br>";
-quiz.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+C+"</label><br><br>";
-quiz.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
-}
+function showForm() {
+    document.getElementById("submitscore").style.display = "block";
+};
 
-function checkAnswer(){
-    // use getElementsByName because we have an array which it will loop through
-    answers = document.getElementsByName("answers");
-    for(var i=0; i<answers.length; i++){
-      if(answers[i].checked){
-        answer = answers[i].value;
-      }
-    }
-    // checks if answer matches the correct choice
-    if(answer == questions[progress].answer){
-      //each time there is a correct answer this value increases
-      correct++;
-    }
-    // changes position of which character user is on
-    progress++;
-    // then the renderQuestion function runs again to go to next question
-    createQuiz();
-  }
-  window.addEventListener("load", createQuiz);
+var subbut = document.getElementById("subHS");
+
+subbut.addEventListener('click', function(event){
+    event.preventDefault()
+    window.location = "highscore.html"
+    
+})
+//timer here
